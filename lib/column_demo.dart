b78ccfab69dc59573349f37761f96b03c3ab1437
anghielo <user_noreply@github.com>
2021-09-09 17:16:29 -0700

    Update

---
+++ lib/column_demo.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ColumnDemoPage extends StatefulWidget {
+  const ColumnDemoPage({Key? key}) : super(key: key);
+
+  @override
+  _ColumnDemoPageState createState() => _ColumnDemoPageState();
+}
+
+class _ColumnDemoPageState extends State<ColumnDemoPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
